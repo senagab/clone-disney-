119dc4cd52c0f68be5a5ea70022a0dafe5e98913
senagab <senagab@gmail.com>
2023-11-29 18:50:28 -0400

    feat: hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     gulp.src('./src/styles/*.scss') // recupera os arquivos .scss 
         .pipe(sass({ outputStyle: 'compressed'})) // comprime os arquivos
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./dist/css')) // destina a pasta
 }
 
-exports.default = styles;
+function images() {
+    gulp.src('./src/images/**/*') // recupera todas as subpastas e seus arquivos
+        .pipe(imagemin()) // minifica os arquivos
+        .pipe(gulp.dest('./dist/images')); // destina a pasta
+}
+
+exports.default = gulp.parallel(styles, images)
+
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
