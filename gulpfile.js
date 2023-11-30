542a4e56b921f3feaf2a8a67063b5c363aae7c83
senagab <senagab@gmail.com>
2023-11-29 20:43:00 -0400

    feat: sessão de listagem de planos

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
 function styles() {
-    gulp.src('./src/styles/*.scss') // recupera os arquivos .scss 
+    return gulp.src('./src/styles/*.scss') // recupera os arquivos .scss 
         .pipe(sass({ outputStyle: 'compressed'})) // comprime os arquivos
         .pipe(gulp.dest('./dist/css')) // destina a pasta
 }
 
 function images() {
-    gulp.src('./src/images/**/*') // recupera todas as subpastas e seus arquivos
+    return gulp.src('./src/images/**/*') // recupera todas as subpastas e seus arquivos
         .pipe(imagemin()) // minifica os arquivos
         .pipe(gulp.dest('./dist/images')); // destina a pasta
 }
